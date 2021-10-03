a20268f54be9a884c6ff20c381045635cd8b6e15
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2021-10-03 15:33:21 +0300

    Labor2 munkam

--- KotlinBasics/Labor2/src/extensions/Main.kt
+++ KotlinBasics/Labor2/src/extensions/Main.kt
@@ -5,9 +5,22 @@ package extensions
 //igy megirok a Stringnek egy kiterjesztett fuggvenyt, kiegeszitek egy uj fuggvennyel!
 fun String.monogram() = split(" ").map { it[0] }.joinToString ("")
 
-fun List<String>.longestString() = forEach{ }
+fun List<String>.joinBySeparator(separator: Char) = map { it }.joinToString("$separator")
+
+fun List<String>.longestString() = maxBy { it.length }
 
 fun main(){
+    println("------------Problem 2----------------------")
+    print("a) alpont: ")
     val name = "John Smith"
     println("$name -> ${name.monogram()}")
+    /**-------------------------------------------------**/
+    print("b) alpont: List with separator: ")
+    val list = listOf("apple","pear","melon");
+    println(list.joinBySeparator('#'))
+
+    /**-------------------------------------------------**/
+    print("c) alpont: Longest string in the list is: ")
+    val list2 = listOf("apple", "pear", "strawberry", "melon")
+    println(list2.longestString())
 }
