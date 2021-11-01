3f93d64ecc2d607a7bcbee62440f8a10ead13169
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2021-11-02 01:27:22 +0200

    QuizApp Labor6

--- QuizAppLabor6/app/src/main/java/com/example/quizapplabor6/models/SharedViewModel.kt
+++ QuizAppLabor6/app/src/main/java/com/example/quizapplabor6/models/SharedViewModel.kt
@@ -11,6 +11,7 @@ class SharedViewModel : ViewModel() {
     private var currentQuestionID = 0
     private var playerName = ""
     private var highScore = 0
+    var currentPosition = 0
 
     fun updateHighScore(): Boolean {
         if (highScore < currentpoints) {
--- QuizAppLabor6/app/src/main/java/com/example/quizapplabor6/ui/questions/QuestionListFragment.kt
+++ QuizAppLabor6/app/src/main/java/com/example/quizapplabor6/ui/questions/QuestionListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizapp.R
@@ -24,11 +25,14 @@ private const val ARG_PARAM2 = "param2"
  * Use the [QuestionListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class QuestionListFragment : Fragment() {
+class QuestionListFragment : Fragment(), QuizAdapter.OnItemClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private val viewModel: SharedViewModel by activityViewModels()
+    private lateinit var list: List<MyItem>
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: QuizAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,17 +58,18 @@ class QuestionListFragment : Fragment() {
     }
 
     private fun initializeRecyclerView(view: View) {
-        val list = uploadList(viewModel.getController().questions.size)
+        list = uploadList(viewModel.getController().questions.size)
+        adapter = QuizAdapter(list,this)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-
-        recyclerView.adapter = QuizAdapter(list)
+        //itt fogom inicializalni a RecyclerView-ot
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.setHasFixedSize(true)
     }
 
     //ez a fuggveny fel fogja tolteni egy listaban a kerdeseket es a hozza tartozo helyes valaszokat
-    private fun uploadList(size: Int): List<MyItem> {
+    private fun uploadList(size: Int): ArrayList<MyItem> {
 
         val list = ArrayList<MyItem>()
         val quizController = viewModel.getController()
@@ -90,7 +95,7 @@ class QuestionListFragment : Fragment() {
 
             list += item
         }
-        //list += MyItem("asd","asd")
+
         return list
     }
 
@@ -112,5 +117,16 @@ class QuestionListFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onDetailsClick(position: Int) {
+        viewModel.currentPosition = position
+        findNavController().navigate(R.id.action_questionListFragment_to_questionDetailFragment)
+
+    }
+
+    override fun onDeleteClick(position: Int) {
+        viewModel.getController().questions.removeAt(position)
+        adapter.notifyItemRemoved(position)
     }
 }
