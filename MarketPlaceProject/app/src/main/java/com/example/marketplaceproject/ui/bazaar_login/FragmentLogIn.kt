89e7dc991b30e756e0c338f023891f9c0be7e6d1
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2021-11-15 23:41:16 +0200

    Android Projekt

--- MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_login/FragmentLogIn.kt
+++ MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_login/FragmentLogIn.kt
@@ -1,12 +1,10 @@
-package com.example.marketplaceproject.bazaar_register
+package com.example.marketplaceproject.ui.bazaar_login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import com.example.marketplaceproject.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,14 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentRegister.newInstance] factory method to
+ * Use the [FragmentLogIn.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentRegister : Fragment() {
+class FragmentLogIn : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var login_hiperlink: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,24 +35,7 @@ class FragmentRegister : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_register, container, false)
-
-        view?.apply {
-            initializeView(this)
-            initializeListeners(this)
-        }
-
-        return view;
-    }
-
-    private fun initializeListeners(view: View) {
-        login_hiperlink.setOnClickListener{
-            findNavController().navigate(R.id.action_fragmentRegister_to_fragmentLogIn)
-        }
-    }
-
-    private fun initializeView(view: View) {
-        login_hiperlink = view.findViewById(R.id.register_login_hiperlink)
+        return inflater.inflate(R.layout.fragment_log_in, container, false)
     }
 
     companion object {
@@ -65,12 +45,12 @@ class FragmentRegister : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentRegister.
+         * @return A new instance of fragment FragmentLogIn.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentRegister().apply {
+            FragmentLogIn().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_register/FragmentRegister.kt
+++ MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_register/FragmentRegister.kt
@@ -1,10 +1,12 @@
-package com.example.marketplaceproject.bazaar_login
+package com.example.marketplaceproject.ui.bazaar_register
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.marketplaceproject.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,13 +16,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentLogIn.newInstance] factory method to
+ * Use the [FragmentRegister.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentLogIn : Fragment() {
+class FragmentRegister : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var login_hiperlink: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +38,24 @@ class FragmentLogIn : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        val view =  inflater.inflate(R.layout.fragment_register, container, false)
+
+        view?.apply {
+            initializeView(this)
+            initializeListeners(this)
+        }
+
+        return view;
+    }
+
+    private fun initializeListeners(view: View) {
+        login_hiperlink.setOnClickListener{
+            findNavController().navigate(R.id.action_fragmentRegister_to_fragmentLogIn)
+        }
+    }
+
+    private fun initializeView(view: View) {
+        login_hiperlink = view.findViewById(R.id.register_login_hiperlink)
     }
 
     companion object {
@@ -45,12 +65,12 @@ class FragmentLogIn : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentLogIn.
+         * @return A new instance of fragment FragmentRegister.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentLogIn().apply {
+            FragmentRegister().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
