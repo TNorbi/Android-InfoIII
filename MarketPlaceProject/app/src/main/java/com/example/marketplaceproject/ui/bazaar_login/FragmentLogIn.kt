f2f987b20674f524b00f18171f0d26dc7c6f6688
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2021-11-16 00:33:59 +0200

    Android Projekt

--- MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_login/FragmentLogIn.kt
+++ MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_login/FragmentLogIn.kt
@@ -1,5 +1,7 @@
 package com.example.marketplaceproject.ui.bazaar_login
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.marketplaceproject.R
 
@@ -24,6 +27,7 @@ class FragmentLogIn : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var title : TextView
     private lateinit var email_input : EditText
     private lateinit var password_input: EditText
     private lateinit var loginButton: Button
@@ -46,6 +50,7 @@ class FragmentLogIn : Fragment() {
 
         view?.apply {
             initializeView(this)
+            changeColorOfTitle()
             initializeListeners(this)
         }
 
@@ -64,10 +69,23 @@ class FragmentLogIn : Fragment() {
     }
 
     private fun initializeView(view: View) {
+        title = view.findViewById(R.id.logInTitle)
         email_input = view.findViewById(R.id.emailInput)
         password_input = view.findViewById(R.id.passwordInput)
         loginButton  = view.findViewById(R.id.logInButton)
         registerButton = view.findViewById(R.id.login_registerButton)
+    }
+
+    private fun changeColorOfTitle() {
+        //ez a kicsi kod megnezi, hogy a telefonunk night modban van vagy sem
+        //ha night modban van akkor a text color feher lesz (hogy konyebben lehessen latni), ellenkezo esetben fekete szin marad (forras : Stack)
+        val nightModeFlags = requireContext().resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK
+        when (nightModeFlags) {
+            Configuration.UI_MODE_NIGHT_YES -> title.setTextColor(Color.parseColor("white"))
+            Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> title.setTextColor(
+                Color.parseColor("black"))
+        }
     }
 
     companion object {
--- MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_register/FragmentRegister.kt
+++ MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/ui/bazaar_register/FragmentRegister.kt
@@ -1,5 +1,7 @@
 package com.example.marketplaceproject.ui.bazaar_register
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -25,11 +27,13 @@ class FragmentRegister : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var firstName_input : EditText
-    private lateinit var lastName_input : EditText
-    private lateinit var email_input : EditText
-    private lateinit var password_input : EditText
-    private lateinit var registerButton : Button
+    private lateinit var title: TextView
+    private lateinit var registerButtomText: TextView
+    private lateinit var firstName_input: EditText
+    private lateinit var lastName_input: EditText
+    private lateinit var email_input: EditText
+    private lateinit var password_input: EditText
+    private lateinit var registerButton: Button
     private lateinit var login_hiperlink: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,10 +49,11 @@ class FragmentRegister : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
 
         view?.apply {
             initializeView(this)
+            changeColorOfTextView()
             initializeListeners(this)
         }
 
@@ -56,18 +61,38 @@ class FragmentRegister : Fragment() {
     }
 
     private fun initializeListeners(view: View) {
-        login_hiperlink.setOnClickListener{
+        login_hiperlink.setOnClickListener {
             findNavController().navigate(R.id.action_fragmentRegister_to_fragmentLogIn)
         }
     }
 
     private fun initializeView(view: View) {
+        title = view.findViewById(R.id.registerTitle)
+        registerButtomText = view.findViewById(R.id.register_bottom_text)
         login_hiperlink = view.findViewById(R.id.register_login_hiperlink)
         firstName_input = view.findViewById(R.id.register_irstName_input)
         lastName_input = view.findViewById(R.id.register_lastName_input)
         email_input = view.findViewById(R.id.register_email_input)
         password_input = view.findViewById(R.id.register_password_input)
         registerButton = view.findViewById(R.id.register_button)
+    }
+
+    private fun changeColorOfTextView() {
+        //ez a kicsi kod megnezi, hogy a telefonunk night modban van vagy sem
+        //ha night modban van akkor a text color feher lesz (hogy konyebben lehessen latni), ellenkezo esetben fekete szin marad (forras : Stack)
+        val nightModeFlags = requireContext().resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK
+        when (nightModeFlags) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                title.setTextColor(Color.parseColor("white"))
+                registerButtomText.setTextColor(Color.parseColor("white"))
+            }
+
+            Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                title.setTextColor(Color.parseColor("black"))
+                registerButtomText.setTextColor(Color.parseColor("black"))
+            }
+        }
     }
 
     companion object {
