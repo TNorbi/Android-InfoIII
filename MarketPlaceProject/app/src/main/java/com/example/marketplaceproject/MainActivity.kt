902d6d52784357d8fed27c5e343fd4c41ae39d8f
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2021-11-27 20:47:33 +0200

    Android Projekt

--- MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/MainActivity.kt
+++ MarketPlaceProject/app/src/main/java/com/example/marketplaceproject/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.marketplaceproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -16,10 +20,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initializeToolBar()
         initializeNavigation()
         setupNavigationMenuVisibility()
     }
 
+    //-----------------Toolbar resz---------------------------------------
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemView = item.itemId
+
+        when(itemView){
+            R.id.search -> Toast.makeText(this,"Search clicked",Toast.LENGTH_SHORT).show()//ide jonne a Search ablak
+            R.id.filter ->Toast.makeText(this,"Filter clicked",Toast.LENGTH_SHORT).show()
+            R.id.profile ->Toast.makeText(this,"Profile clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        return false
+    }
+
+    private fun initializeToolBar(){
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+    }
+
+    //------------------------------Bottom Navigation resz----------------------------
     private fun initializeNavigation(){
         navigationView = findViewById(R.id.bottom_navigation)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
